b24a413d9f77436b3e9a9c154cae6488cb2d1117
supermarket-ie <colin@supermarket.ie>
2026-08-18 14:40:13 +0100

    fix Eve send icon path

--- src/components/HomePlanner.tsx
+++ src/components/HomePlanner.tsx
@@ -218,7 +218,7 @@ function EvePlannerInner({ saved, storageKey }: { saved: SavedEveChat; storageKe
             style={{ background: '#00944A' }}
           >
             <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth={2.5}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
             </svg>
           </button>
         </form>
